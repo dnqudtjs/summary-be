c4ebdde8c0a9d00885d17ebaf093a657ef4c7956
woo <dnqudtjs@gmail.com>
2021-03-25 15:59:30 +0900

    init

---
+++ src/main/java/com/summarybe/SummaryBeApplication.java
@@ -0,0 +1,13 @@
+package com.summarybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummaryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummaryBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/summarybe/SummaryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summarybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummaryBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
